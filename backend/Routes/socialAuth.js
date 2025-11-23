d1317671b7887f7357cf1bb468db61752ba6bad2
housamhegazy <geohousam@gmail.com>
2025-11-23 21:44:50 +0300

    update

--- backend/Routes/socialAuth.js
+++ backend/Routes/socialAuth.js
@@ -8,8 +8,8 @@ function setAuthCookie(res, token) {
   // إعداد الكوكيز مع الخيارات المناسبة
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
+    secure: true, //process.env.NODE_ENV === "production"
+    sameSite: "None",//process.env.NODE_ENV === "production" ? "Strict" : "Lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 أسبوع
   });
 }
